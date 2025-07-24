d3000449b35739025046b72b524ae6b511f3346b
test <test@example.com>
2025-07-25 02:35:36 +0900

    22

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/typing-game/",
+  base: "/typing-game/dist/",
   build: {
     outDir: "docs",
   },
